d26c5823216326a512310de172d4a9b7dbf02bd1
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-08-05 14:39:35 +0300

    Update lawyers_provider.dart

--- lib/features/lawyers/presentation/providers/lawyers_provider.dart
+++ lib/features/lawyers/presentation/providers/lawyers_provider.dart
@@ -18,7 +18,8 @@ Future<List<LawyerProfile>> lawyersList(LawyersListRef ref) {
 
   return repository.getLawyers().then((lawyers) {
     if (category == null) return lawyers;
-    return lawyers.where((l) => l.specializations.contains(category)).toList();
+  return lawyers.where((l) => l.specialization?.contains(category) == true).toList();
+
   });
 }
 
