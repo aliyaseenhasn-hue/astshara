5cf7e1a5b4a9cdf1b062f5b8e790d5bdcd90c3bf
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-08-12 01:10:33 +0300

    ui: make shared loading indicator theme aware

--- lib/shared/widgets/loading_widget.dart
+++ lib/shared/widgets/loading_widget.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-import '../../core/constants/app_colors.dart';
 
 class LoadingWidget extends StatelessWidget {
   final double size;
+
   const LoadingWidget({super.key, this.size = 40.0});
 
   @override
   Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
     return Center(
       child: SizedBox(
         width: size,
         height: size,
-        child: const CircularProgressIndicator(
-          color: AppColors.primary,
+        child: CircularProgressIndicator(
+          color: scheme.primary,
           strokeWidth: 3,
         ),
       ),
