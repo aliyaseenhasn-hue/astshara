c7f4aaaa93745affce367290b6fcb6cacf7de02d
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-08-21 21:20:23 +0300

    perf: prepare lawyer repository pagination

--- lib/features/lawyers/domain/repositories/lawyers_repository.dart
+++ lib/features/lawyers/domain/repositories/lawyers_repository.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 import '../entities/lawyer_profile.dart';
 
 abstract class LawyersRepository {
-  Future<List<LawyerProfile>> getLawyers();
+  Future<List<LawyerProfile>> getLawyers({int limit = 20, int offset = 0});
   Future<LawyerProfile?> getLawyerProfile(String profileId);
   Future<void> updateLawyerProfile(LawyerProfile profile);
   Future<String> uploadFile(Uint8List bytes, String fileName, String bucket);
