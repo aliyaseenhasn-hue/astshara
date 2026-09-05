d9b4c000df2ec518f478ac9376a91581055a4546
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-09-06 02:43:56 +0300

    fix: import go_router for lawyer availability redirect

--- lib/features/lawyers/presentation/pages/lawyer_availability_page.dart
+++ lib/features/lawyers/presentation/pages/lawyer_availability_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:go_router/go_router.dart';
 import 'package:astshara/core/config/supabase_config.dart';
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/utils/app_time_format.dart';
