4b1fc8528ff46830bafbea2ff917a4cac9db07cd
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-09-06 23:21:14 +0300

    fix: import Supabase auth event types explicitly

--- lib/core/services/push_notification_service.dart
+++ lib/core/services/push_notification_service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../config/supabase_config.dart';
 import 'notification_service.dart';
