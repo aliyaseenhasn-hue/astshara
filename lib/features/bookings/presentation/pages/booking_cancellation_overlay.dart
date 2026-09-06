c1d164a34bb88eee9b38bca7352b62df1030f8f8
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-09-06 18:42:16 +0300

    fix: import go_router for booking action navigation

--- lib/features/bookings/presentation/pages/booking_cancellation_overlay.dart
+++ lib/features/bookings/presentation/pages/booking_cancellation_overlay.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:astshara/core/config/supabase_config.dart';
 import '../../../authentication/presentation/providers/auth_provider.dart';
 import '../providers/bookings_provider.dart';
